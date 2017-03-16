58e7cdd8fa0b8b184a672039a76613722f22800e
zeroem01 <zeroem01@gmail.com>
2017-03-16 13:58:29 -0400

    Fun with Hash

--- recipes/hi-file-sub.rb
+++ recipes/hi-file-sub.rb
@@ -1,7 +1,10 @@
-template '/home/ec2-user/hi.txt' do
+template '/home/ec2-user/hi-sub.txt' do
   source 'hi-sub.erb'
   owner 'ec2-user'
   group 'ec2-user'
   mode '0755'
-  variables ('boom_sub': 'boom' )
+  variables ({
+    :ip => node['ipaddress'],
+    :fqdn => node['fqdn']
+    })
 end
