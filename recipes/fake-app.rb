8349e6e104bd50886d7626329076ce6eb3ff962b
zeroem01 <zeroem01@gmail.com>
2017-03-17 09:42:53 -0400

    fake app

--- recipes/fake-app.rb
+++ recipes/fake-app.rb
@@ -1,5 +1,5 @@
-file '/home/ec2-user/fake-app.txt' do
-  path '/files/fake-app.txt'
+cookbook_file '/home/ec2-user/fake-app.txt' do
+  path 'fake-app.txt'
   mode '0755'
   owner 'ec2-user'
   group 'ec2-user'
